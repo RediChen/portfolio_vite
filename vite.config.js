c74684259c2bcf6a895432b158ded6722a14fb1c
RediChen <a0929052958@gmail.com>
2024-10-08 19:09:12 +0800

    發布前置作業

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio_vite/',
   plugins: [react()],
   css: {
     preprocessorOptions: {
