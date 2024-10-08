aa994457be758905a0ff978c50035f9f290e9486
RediChen <a0929052958@gmail.com>
2024-10-09 00:10:41 +0800

    建立 GitHub 連結鈕樣式

--- src/panel-main/1-basic/core.jsx
+++ src/panel-main/1-basic/core.jsx
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
 import s from './core.module.scss';
 import { IoCaretDownOutline } from "react-icons/io5";
+import { FaGithub } from "react-icons/fa";
 
 const Core = ({ setOpenPortfolio = () => { } }) => {
   return (
@@ -15,8 +16,10 @@ const Core = ({ setOpenPortfolio = () => { } }) => {
         <caption className={s.caption}>
           <h1><span>陳</span><span>致</span></h1>
         </caption>
+        <a href="https://github.com/RediChen" target="_blank" className={s.ghBtn}>
+          <FaGithub />
+        </a>
       </figure>
-      <button>GitHub Link</button>
       <button className={s.portfolioBtn} onClick={() => setOpenPortfolio(true)}>
         <h3>作品集</h3>
         <i><IoCaretDownOutline /></i>
